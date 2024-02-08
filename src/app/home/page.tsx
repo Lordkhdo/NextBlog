5459ff6196cd3d57486207ccb379b29ec567276e
Gabriel Esteves <gabrielesteves51234@gmail.com>
2024-02-08 16:06:59 -0300

    projeto zero

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,22 +2,12 @@
 import { useState } from 'react';
 import styles from './page.module.css'
 const home = () => {
-    const [testNumber , setTestNumber] = useState(false)
-    const [Classes,setClasses] = useState([styles.section ,styles.active ])
-    const mudarCor = () =>{
-        if(testNumber){
-            setTestNumber(false)
-            return
-        }
-        setTestNumber(true)
-    }   
+  
     return ( 
         <>
-            {testNumber ?
-            <p  className={`${styles.section}`} >gabreiel dos santos eteves</p>:
-            <p  className={`${styles.section} ${styles.active}`} >gabreiel dos santos eteves</p>
-            }
-            <button onClick={()=> mudarCor()}>dasdasdas </button>
+            <p className={`${styles.section}`}>
+                <span className={`${styles.span}`}>teste123</span>
+            </p>
         </>
      );
 }
